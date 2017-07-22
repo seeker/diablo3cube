f17f5fdc500034b5afcc0187bd6e00fa89dfd10f
Nicholas Wright <dozedoffagain@gmail.com>
2017-07-22 13:48:46 +0200

    Service to provide items

---
+++ src/app/item.service.spec.ts
@@ -0,0 +1,32 @@
+import { async, inject, TestBed } from '@angular/core/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+
+import { ItemService } from './item.service';
+import { Item } from './item';
+
+describe('ItemService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ItemService]
+    });
+  });
+
+  it('should be created', inject([ItemService], (service: ItemService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  it('armors should contain', inject([ItemService], (service: ItemService) => {
+    service.getArmors().then((res) => {expect(res)
+      .toContain(new Item("Beckon Sail", "When receiving fatal damage, you instead automatically cast Smoke Screen and are healed to 25% Life. This effect may occur once every 120 seconds."))});
+  }));
+
+  it('jewelry should contain', inject([ItemService], (service: ItemService) => {
+    service.getJewelry().then((res) => {expect(res)
+    .toContain(new Item("Ancestors' Grace", "When receiving fatal damage, you are instead restored to 100% of maximum Life and resources. This item is destroyed in the process."))});
+  }));
+
+  it('weapons should contain', inject([ItemService], (service: ItemService) => {
+    service.getWeapons().then((res) => {expect(res)
+      .toContain(new Item("Aether Walker", "Teleport no longer has a cooldown but costs 25 Arcane Power."))});
+  }));
+});
---
+++ src/app/item.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Item } from './item';
+
+import 'rxjs/add/operator/map';
+
+var itemData = require('assets/items.json')
+
+@Injectable()
+export class ItemService {
+  private itemStore = 'assets/items.json'
+
+  private armors : Item[];
+  private jewelry : Item[];
+  private weapons : Item[];
+
+  constructor() {
+    this.armors = this.createItems(itemData.armor);
+    this.jewelry = this.createItems(itemData.jewelry);
+    this.weapons = this.createItems(itemData.weapons);
+  }
+
+  getArmors(): Promise<Item[]> {
+    return Promise.resolve(this.armors);
+  }
+
+  getJewelry(): Promise<Item[]> {
+    return Promise.resolve(this.jewelry);
+  }
+
+  getWeapons(): Promise<Item[]> {
+    return Promise.resolve(this.weapons);
+  }
+
+  private createItems(jsonArray:any) : Item[] {
+    console.log(jsonArray);
+
+    let result : Item[] = [];
+
+    for(let row of jsonArray) {
+      result.push(new Item(row.name, row.affix));
+    }
+
+    return result;
+  }
+}

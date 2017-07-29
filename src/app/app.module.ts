52907d379c104ad45538c93e3f3239ae76e148b8
Nicholas Wright <dozedoffagain@gmail.com>
2017-07-29 18:43:25 +0200

    Checkboxes for item state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LocalStorageModule } from 'angular-2-local-storage';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
 
 import {ItemService} from './item.service';
+import { CubeItemService } from './cube-item.service';
 
 @NgModule({
   declarations: [
@@ -16,12 +18,13 @@ import {ItemService} from './item.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     LocalStorageModule.withConfig({
     prefix: 'diablo3cube',
     storageType: 'localStorage'
     })
   ],
-  providers: [LocalStorageModule, ItemService],
+  providers: [LocalStorageModule, ItemService, CubeItemService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import {CubeItemService} from '../cube-item.service';
+import { CubeItemService } from '../cube-item.service';
 
-import {Item} from '../item';
-import {CubeItem} from '../cube-item';
+import { Item } from '../item';
+import { CubeItem } from '../cube-item';
 
 @Component({
   selector: 'app-item-detail',
@@ -11,12 +11,37 @@ import {CubeItem} from '../cube-item';
   styleUrls: ['./item-detail.component.css']
 })
 
-export class ItemDetailComponent implements OnInit {
-  @Input() item: Item;
+export class ItemDetailComponent {
+  private localItem: Item;
+  private cube: CubeItem;
 
-  constructor() {
+  @Input()
+  set item(item: Item) {
+    console.log('Item ' + item.name + ' set');
+    this.localItem = item;
+    this.loadCubeData();
   }
 
-  ngOnInit() {
+  private loadCubeData(): void {
+    const savedState: CubeItem = this.cubeItemService.get(this.localItem.name);
+
+    if (savedState === undefined) {
+      console.log('No entry found for ' + this.localItem.name + ', creating new entry');
+      this.cube = new CubeItem(this.localItem.name);
+    } else {
+      console.log('Entry found for ' + this.localItem.name);
+      this.cube = savedState;
+    }
+  }
+
+  get item(): Item {
+    return this.localItem;
+  }
+
+  constructor(private cubeItemService: CubeItemService) {
+  }
+
+  checkboxChanged() {
+    this.cubeItemService.set(this.cube);
   }
 }
